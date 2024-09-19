013ad6348885a2617fe01fdfd2ca17dae4d6d0c7
xankush <ankushdohare12@gmail.com>
2024-09-19 19:07:50 +0530

    code to tell which day on a particular date of month of year

---
+++ ankush.java/day.java
@@ -0,0 +1,46 @@
+
+import java.util.Calendar;
+import java.util.*;
+
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+      Calendar cal = Calendar.getInstance();
+      cal.set(Calendar.DAY_OF_MONTH, day);
+      cal.set(Calendar.MONTH, month-1);
+      cal.set(Calendar.YEAR, year);
+      int i =  cal.get(Calendar.DAY_OF_WEEK);
+      String[] str = {"SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"};
+      return str[i-1];
+    }
+
+}
+
+public class day {
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      System.out.print("enter date: ");
+      int day = sc.nextInt();
+      System.out.print("enter month: ");
+      int month = sc.nextInt();  
+      System.out.print("enter year: ");
+      int year = sc.nextInt();  
+    
+      String ans = Result.findDay(month, day, year);
+
+        System.out.println(ans);
+ 
+        
+    }
+}
